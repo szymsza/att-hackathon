53b8ba46b02caddd161de5035f9738c89c914cf0
Jakub Szymsza <jakub@szymsza.cz>
2019-11-22 19:06:06 +0100

    API middleware added

--- routes/api.php
+++ routes/api.php
@@ -1,18 +1,18 @@
 <?php
 
-use Illuminate\Http\Request;
-
 /*
 |--------------------------------------------------------------------------
-| API Routes
+| Web Routes
 |--------------------------------------------------------------------------
 |
-| Here is where you can register API routes for your application. These
+| Here is where you can register web routes for your application. These
 | routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
+| contains the "web" middleware group. Now create something great!
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
+
+	Route::get('user', function (Request $request) {
+    	return "Api route";
+	});
+
